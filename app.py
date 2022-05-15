0f39543fda494f1f344f07135c5c528ca3d65cc8
Vincent <vickjoeobigwe@gmail.com>
2022-05-15 06:09:32 +0200

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 # Creating FastAPI instance
 app = FastAPI()
 
-#creating class to define the request body
+# creating class to define the request body
 # and the type hints of each attribute
 
 
@@ -35,13 +35,15 @@ class request_body(BaseModel):
     Equipment_Sensors3: int
     Efficiency3: int
 
+
 # Loading Dataset
 truckData = pd.read_csv("truckData.csv")
 
 df = pd.DataFrame(truckData)
 
 # Header
-names = ['Distance_Covered', 'Match_of_Route', 'Fuel_Consumption', 'Body_Characteristics', 'Equipment_Sensors', 'Efficiency', 'Rating']
+names = ['Distance_Covered', 'Match_of_Route', 'Fuel_Consumption',
+         'Body_Characteristics', 'Equipment_Sensors', 'Efficiency', 'Rating']
 
 X, Y = df.iloc[:, :-1], df.iloc[:, [-1]]
 
@@ -51,6 +53,7 @@ clf.fit(X, Y)
 
 # Creating an Endpoint to receive the data
 # to make prediction on.
+
 
 @app.post('/predict')
 def predict(data: request_body):
@@ -64,32 +67,32 @@ def predict(data: request_body):
         data.Efficiency,
     ]]
     test_data2 = [[
-        data.Distance_Covered,
-        data.Match_of_Route,
-        data.Fuel_Consumption,
-        data.Body_Characteristics,
-        data.Equipment_Sensors,
-        data.Efficiency,
+        data.Distance_Covered2,
+        data.Match_of_Route2,
+        data.Fuel_Consumption2,
+        data.Body_Characteristics2,
+        data.Equipment_Sensors2,
+        data.Efficiency2,
     ]]
     test_data3 = [[
-        data.Distance_Covered,
-        data.Match_of_Route,
-        data.Fuel_Consumption,
-        data.Body_Characteristics,
-        data.Equipment_Sensors,
-        data.Efficiency,
+        data.Distance_Covered3,
+        data.Match_of_Route3,
+        data.Fuel_Consumption3,
+        data.Body_Characteristics3,
+        data.Equipment_Sensors3,
+        data.Efficiency3,
     ]]
 
     # Making the prediction
     result1 = clf.predict(test_data)[0]
     result2 = clf.predict(test_data2)[0]
     result3 = clf.predict(test_data3)[0]
-    
+
     # Map the prediction to the rating
     rating1 = 0
     rating2 = 0
     rating3 = 0
-    
+
     if result1 == 'Highest':
         rating1 = 4
     elif result1 == 'High':
@@ -100,7 +103,7 @@ def predict(data: request_body):
         rating1 = 1
     else:
         rating1 = 0
-    
+
     if result2 == 'Highest':
         rating2 = 4
     elif result2 == 'High':
@@ -111,7 +114,7 @@ def predict(data: request_body):
         rating2 = 1
     else:
         rating2 = 0
-        
+
     if result3 == 'Highest':
         rating3 = 4
     elif result3 == 'High':
@@ -125,10 +128,10 @@ def predict(data: request_body):
 
     # Return the max rating
     if (rating1 >= rating2) and (rating1 >= rating3):
-        return data.TruckName2 + ':' + ' ' + result2 
+        return data.TruckName + ':' + ' ' + result1
     elif (rating2 >= rating1) and (rating2 > rating3):
         return data.TruckName2 + ':' + ' ' + result2
     elif (rating3 >= rating1) and (rating3 >= rating2):
-        return data.TruckName2 + ':' + ' ' + result3
+        return data.TruckName3 + ':' + ' ' + result3
     else:
-        return data.TruckName2 + ':' + ' ' + result2
+        return data.TruckName + ':' + ' ' + result1
